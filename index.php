11fa91d0c0f9415cce932e248d0c6d6d55a0cafd
David Tofigh <david.tofigh7@gmail.com>
2018-04-12 15:01:03 +0200

    Modification site  Selest

--- index.php
+++ index.php
@@ -2,70 +2,94 @@
         <div class="img-background">
                 <div class="container">
                         <div class="row">
-                                <div class="col"></div>
-                                        <div class="border border-secondary col zone-avantage">
-                                                <h2 class="titre-avantage">Les Avantages</h2>
-                                                <p>Lorem ipsum dolor sit amet</p>
-                                                <p>consectetuer adipiscing elit</p>
-                                                <p>commodo ligula eget dolor</p>
+                                <div class=""></div>
+                                        <div class="zone-avantage">
+                                                <strong class="titre-avantage">Les Avantages</strong>
+                                                <ul>
+                                                        <li><p>Lorem ipsum dolor sit amet</p></li>
+                                                        <li><p>Lorem ipsum dolor sit amet</p></li>
+                                                        <li><p>Lorem ipsum dolor sit amet</p></li>
+                                                </ul>
                                         </div>
                                 </div>
                         </div>
                 </div>
         </div>
+<section id="service" class="zone-service">
         <div class="container">
-                <div class="border border-success jumbotron" style="margin-top:15px">
-                <h3 style="text-align : center; color : #0A71B4"> Acces direct </h3>
-                <hr class="my-4">
+                <div class="section">
+                        <h2 class="txt-titre-service">ENTRAIDE, SERVICES ET OBJETS ENTRE VOISINS</h2>
+                        <h3 class="txt-sous-titre-service">Un seul réseau pour partager et faire des économies</h3>
+                </div>
                 <div class="row">
-                        <div class="col"></div>
-                        <div class="col">
-                                <img src="img/contenu/test_small_img.png">
+                        <div class="col-lg-12">
+                                <p class="btn-service">
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-tags fa-2x"></i><br>
+                                                Petites<br>annonces
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-car fa-2x"></i><br>Auto partage<br>Covoiturage
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-wrench fa-2x"></i><br>
+                                                Prêt<br>d'objets
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-envira fa-2x"></i><br>
+                                                Jardins<br>partagés
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <span class="icon-shop"></span><br>
+                                                Services de<br>Proximité
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-calendar-check-o fa-2x fa-fw"></i><br>
+                                                Vie<br>locale
+                                        </a>
+                                        <a href="#" class="btn btn-sq  avoid-clicks">
+                                                <i class="fa fa-users fa-2x"></i><br>
+                                                Entraide<br>lien social
+                                        </a>
+                                </p>
                         </div>
-                        <div class="col">
-                                <img src="img/contenu/test_small_img.png">
-                        </div>
-                        <div class="col">
-                                <img src="img/contenu/test_small_img.png">
-                        </div>
-                        <div class="col"></div>
-
-                </div>
-                </div>
-        </div>
-<div class="border border-success"style="background-color : lightgrey">
-<br/>
-<h3 style="text-align : center; color : #0A71B4">
-                                                        Comment ça marche.
-                                                </h3>
-                                                <br/>
-                                                <br/>
-                <div class="container">
-                        <div class="row">
-                        <div class="col">
-                                <h4 style="text-align : center"> Je poste ma demande </h4>
-                        </div>
-                        <div class="col">
-                                <h4 style="text-align : center"><b> Quelqu'un me repond </b></h4>
-                        </div>
-                        <div class="col">
-                                <h4 style="text-align : center"> Le service est rendu </h4>
-                        </div>
-                        
-
-                </div>
-                <br/>
-                                                <br/><br/>
-                                                <br/>
                 </div>
         </div>
-                
+</section>
+<!--        <div class="container">-->
+<!--                <div class="acces-direct">-->
+<!--                        <div class="row ">-->
+<!--                                <div class="col block" id="btn-acc-1">-->
+<!--                                        <a href="#">ACC-1</a>-->
+<!--                                </div>-->
+<!--                                <div class="col block" id="btn-acc-2">-->
+<!--                                        <a href="#">ACC-2</a>-->
+<!--                                </div>-->
+<!--                                <div class="col block" id="btn-acc-3">-->
+<!--                                        <a href="#">ACC-3</a>-->
+<!--                                </div>-->
+<!--                        </div>-->
+<!--                </div>-->
+<!--        </div>-->
+<section class="zone-pub">
+        <h2 class="txt-service-centre">pratique, économique, solidaire</h2>
+</section>
+<section class="zone-marche">
+        <div class="container">
+                <h2>Comment ça marche.</h2>
+                <div class="row">
+                        <div class="col">
+                                <p>Je poste ma demande</p>
+                        </div>
+                        <div class="col">
+                                <p>Quelqu'un me repond</p>
+                        </div>
+                        <div class="col">
+                                <p>Le service est rendu</p>
+                        </div>
+                </div>
         </div>
-        <div style="background-image:url(img/contenu/accueil_img1.png);">
-                
-        </div>
-
+</section>
    <?php include('templates/footer.html');?>
 </body>
-
 </html>
