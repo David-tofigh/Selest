98d8237ab33f4d2eab71132475e42a2ca59f38ef
David Tofigh <david.tofigh7@gmail.com>
2018-05-14 14:27:35 +0200

    Modification site  Selest

--- page_demande.php
+++ page_demande.php
@@ -1,119 +1,117 @@
 <body>
     <?php include('templates/header.html');?>
     <div class="container-fluid">
-        <div class="row">
-            <nav class="menu border border-secondary jumbotron col-sm-3">
-                <p>Offres</p>
+        <div class="row links">
+            <div class="col-sm-3">
+                <h2>Demandes d'objets</h2>
                 <hr class="my-2">
-                <p class="sub-menu">
-                    Dernières offres
-                </p>
-                <p class="sub-menu">
-                    Catégories offres
-                </p>
-                <p class="sub-menu">
-                    Catégories les plus utilisés
-                </p>
-                <p class="sub-menu">
-                    Poster une offre
-                </p>
-                <p>Demandes</p>
-                <hr class="my-2">
-                <p class="sub-menu">
-                    Dernières demandes
-                </p>
-                <p class="sub-menu">
-                    Catégories des demandes
-                </p>
-                <p class="sub-menu">
-                    Catégories les plus utilisés
-                </p>
-                <p class="sub-menu">
-                    Poster une demande
-                </p>
-            </nav>
-
-<section class="card bottom_shadowed_box col-sm-5" style ="margin-left: 12px;">
-        		<div class="card-body">
-            		<h3 class="card-title">Emeline La Fondue</h3>
-            		<h4 class="card-text"><b>Description</b></h4>
-            		<h5 class="card-title">Cherche une personne pour venir m'aider à garder les chiens des voisins, ils sont très gentils, et en manque d'amuuur, pliz hailp</h5>
-            		<br/>
-                </div>
-                <div  style = "text-align : right;">
-       		                    <p class="lead">
-                        <b>Categorie : </b> Multiple</p>
-                    <p class="lead">
-                        <b>Souèts : </b> 120(2heures)</p>
-                    <p class="lead">
-                        <b>Date souhaitée : </b> 12/07/2018</p>
-                    <button type="button" class="btn btn-success">Accepter</button>
-                                            <br/>
-
-       		</section>
-            <aside class="card bottom_shadowed_box col-sm-3" style ="margin-left: 12px;">
-        		<img class="card-img-top" src="img/theme/profil.png" alt="Card image cap">
-        		<div class="card-body">
-            		<h5 class="card-title">User Name</h5>
-            		<p class="card-text">Description</p>
-
-            		<p class="card-body">Age : 29</p>
-            		<a href="#" class="btn btn-primary">Voir le profil</a>
-       			</div>
-    		</aside>
-    		
-   		</div>
-       <br/>
-       		
-	  </div>
-
+                <nav class="border border-secondary jumbotron">
+                    <ul>
+                        <li class="">
+                            <a href="">Offres</a>
+                        </li>
+                        <li class="">
+                            <a href="">Dernières offres</a>
+                        </li>
+                        <li class="">
+                            <a href="">Catégories offres</a>
+                        </li>
+                        <li class="">
+                            <a href="">Catégories les plus utilisés</a>
+                        </li>
+                        <li class="">
+                            <a href="">Poster une offre</a>
+                        </li>
+                        <li class="">
+                            <a href="">Dernières demandes</a>
+                        </li>
+                        <li class="">
+                            <a href="">Catégories des demandes</a>
+                        </li>
+                        <li class="">
+                            <a href="">Catégories les plus utilisés</a>
+                        </li>
+                        <li class="">
+                            <a href="">Poster une demande</a>
+                        </li>
+                    </ul>
+                </nav>
             </div>
-
+            <div class="card bottom_shadowed_box col-sm-5">
+                <div class="card-body">
+                    <h2 class="card-title">Emeline La Fondue</h2>
+                    <strong class="card-text">Description</strong>
+                    <p class="card-title">Cherche une personne pour venir m'aider à garder les chiens des voisins, ils sont très gentils, et en manque d'amuuur, pliz hailp</p>
+                </div>
+                <div class="">
+                    <p class="lead">Categorie :
+                        <span>
+                                Multiple
+                            </span>
+                    </p>
+                    <p class="lead">Souèts :
+                        <span>
+                                120(2heures)
+                            </span>
+                    </p>
+                    <p class="lead">Date souhaitée :
+                        <span>
+                                12/07/2018
+                            </span>
+                    </p>
+                    <button type="button" class="btn btn-success">Accepter</button>
+                </div>
+            </div>
+            <div class="card bottom_shadowed_box col-sm-3">
+                <img class="card-img-top" src="img/theme/profil.png" alt="Card image cap">
+                <div class="card-body">
+                    <p class="card-title">User Name</p>
+                    <p class="card-text">Description</p>
+                    <p class="card-body">Age : 29</p>
+                    <a href="#" class="btn btn-primary">Voir le profil</a>
+                </div>
+            </div>
         </div>
     </div>
-
-
-<!-- <div class="row">
-    <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
-        <img class="card-img-top" src="img/femme1.png" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">User Name</h5>
-            <p class="card-text">Description</p>
-            <p class="card-body">Age : 29</p>
-            <a href="#" class="btn btn-primary">Voir le profil</a>
+        <!-- <div class="row">
+            <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
+                <img class="card-img-top" src="img/femme1.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">User Name</h5>
+                    <p class="card-text">Description</p>
+                    <p class="card-body">Age : 29</p>
+                    <a href="#" class="btn btn-primary">Voir le profil</a>
+                </div>
+            </div>
+            <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
+                <img class="card-img-top" src="img/homme1.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">User Name</h5>
+                    <p class="card-text">Description</p>
+                    <p class="card-body">Age : 29</p>
+                    <a href="#" class="btn btn-primary">Voir le profil</a>
+                </div>
+            </div>
+            <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
+                <img class="card-img-top" src="img/femme2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">User Name</h5>
+                    <p class="card-text">Description</p>
+                    <p class="card-body">Age : 29</p>
+                    <a href="#" class="btn btn-primary">Voir le profil</a>
+                </div>
+            </div>
+            <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
+                <img class="card-img-top" src="img/homme2.png" alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">User Name</h5>
+                    <p class="card-text">Description</p>
+                    <p class="card-body">Age : 29</p>
+                    <a href="#" class="btn btn-primary">Voir le profil</a>
+                </div>
+            </div>
         </div>
-    </div>
-    <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
-        <img class="card-img-top" src="img/homme1.png" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">User Name</h5>
-            <p class="card-text">Description</p>
-            <p class="card-body">Age : 29</p>
-            <a href="#" class="btn btn-primary">Voir le profil</a>
-        </div>
-    </div>
-    <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
-        <img class="card-img-top" src="img/femme2.png" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">User Name</h5>
-            <p class="card-text">Description</p>
-            <p class="card-body">Age : 29</p>
-            <a href="#" class="btn btn-primary">Voir le profil</a>
-        </div>
-    </div>
-    <div class="card bottom_shadowed_box col-sm-3" style="width: 18rem;">
-        <img class="card-img-top" src="img/homme2.png" alt="Card image cap">
-        <div class="card-body">
-            <h5 class="card-title">User Name</h5>
-            <p class="card-text">Description</p>
-            <p class="card-body">Age : 29</p>
-            <a href="#" class="btn btn-primary">Voir le profil</a>
-        </div>
-    </div>
-</div>
- -->
-
-
+         -->
     <?php include("templates/footer.html");?>
 </body>
 
